084e875cdc4878c7b693b4c10c9c8dc2f449f66b
CrumbsTrace <45168144+CrumbsTrace@users.noreply.github.com>
2022-11-28 22:03:59 +0100

    Different solution cause why not

--- src/Day1_2021.hs
+++ src/Day1_2021.hs
@@ -1,17 +1,22 @@
 module Day1_2021 (run) where
 
+import Control.Applicative (Alternative (many))
+import Data.Attoparsec.ByteString.Char8 (Parser, decimal, skipSpace)
 import Data.ByteString qualified as BS
-import Utils ( parseNumbers )
+import Utils (runParser)
 
 run :: BS.ByteString -> (Int, Int)
 run input = (p1, p2)
   where
-    numbers = parseNumbers input
+    numbers = runParser numberParser input
     p1 = countIncreases 1 numbers
     p2 = countIncreases 3 numbers
 
 countIncreases :: Int -> [Int] -> Int
-countIncreases offset xs = sum $ zipWith isIncreaseAsInt xs offsetList
-  where
-    isIncreaseAsInt x y = fromEnum (x < y)
-    offsetList = drop offset xs
+countIncreases offset list = length [pair | pair@(x, y) <- pairWithOffset offset list, y > x]
+
+pairWithOffset :: Int -> [b] -> [(b, b)]
+pairWithOffset o xs = zip xs (drop o xs)
+
+numberParser :: Parser [Int]
+numberParser = many $ decimal <* skipSpace
--- src/Utils.hs
+++ src/Utils.hs
@@ -1,19 +1,12 @@
-module Utils(readFromFile, parseNumbers) where
+module Utils (readFromFile, runParser) where
 
-import qualified Data.ByteString as BS
--- import qualified Data.Char as C
-import Data.Attoparsec.ByteString.Char8 as P
-import Control.Applicative
+import Data.Attoparsec.ByteString.Char8 (Parser, parseOnly)
+import Data.ByteString qualified as BS
 
 readFromFile :: FilePath -> IO BS.ByteString
 readFromFile = BS.readFile
 
-numberParser :: Parser [Int]
-numberParser = many $ P.decimal <* P.skipSpace
-
-parseNumbers :: BS.ByteString -> [Int]
-parseNumbers b = case P.parseOnly numberParser b of
-    Left _ -> []
-    Right s -> s
-
-
+runParser :: Parser [a] -> BS.ByteString -> [a]
+runParser p b = case parseOnly p b of
+  Left _ -> []
+  Right s -> s
