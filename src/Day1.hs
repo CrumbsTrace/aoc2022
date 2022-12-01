50409ad0feee80a00acd026d5567c5df3aabf453
CrumbsTrace <45168144+CrumbsTrace@users.noreply.github.com>
2022-12-01 12:55:01 +0100

    Add type signature to run

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,10 +1,11 @@
-{-# OPTIONS_GHC -Wno-missing-signatures #-}
-
 module Day1 (run) where
 
 import Data.Attoparsec.ByteString.Char8 (Parser, char, decimal, sepBy')
+import Data.ByteString (ByteString)
 import Utils (runParser, sortDesc, bstring)
 
+
+run :: ByteString -> (Int, Int)
 run input = (p1, p2)
   where
     parsedInput = runParser parser input
