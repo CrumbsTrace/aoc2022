52d907006db2fc60a6359884fc56f331b9e2e9b8
CrumbsTrace <45168144+CrumbsTrace@users.noreply.github.com>
2022-12-03 16:31:57 +0100

    Some cleanup

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,10 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
-module Day2(run) where
+module Day2 (run) where
 
 import Data.Attoparsec.ByteString.Char8 as P (Parser, many', take)
 import Data.ByteString.Char8 as BS (ByteString, head, last, take)
+import Data.Char (ord)
 import Utils (runParser)
-import Data.Char ( ord )
 
 run :: ByteString -> (Int, Int)
 run input = (p1, p2)
@@ -14,38 +13,39 @@ run input = (p1, p2)
     p2 = sum $ map (getPoints . chooseMove) parsedInput
 
 getPoints :: (Int, Int) -> Int
-getPoints (c1, c2) 
-  | c1 == c2               = c2 + 3 --Draw
-  | c1 - c2 `elem` [-1, 2] = c2 + 6 --Win
-  | otherwise              = c2 -- Loss
+getPoints (c1, c2)
+  | c1 == c2 = c2 + 3 -- Draw
+  | c1 - c2 `elem` [-1, 2] = c2 + 6 -- Win
+  | otherwise = c2 -- Loss
 
 chooseMove :: (Int, Int) -> (Int, Int)
 chooseMove (c1, c2)
-  | c2 == 2   = (c1, c1) -- Draw
-  | c2 == 3   = (c1, makeValid $ c1 - 2) -- Win
+  | c2 == 2 = (c1, c1) -- Draw
+  | c2 == 3 = (c1, makeValid $ c1 - 2) -- Win
   | otherwise = (c1, makeValid $ c1 - 1) -- Loss
 
 makeValid :: Int -> Int
 makeValid c = if c < 1 then c + 3 else c
 
 parser :: Parser [(Int, Int)]
-parser = many' $ (convert . (BS.take 3)) <$> (P.take 4) 
-   
+parser = many' $ convert . BS.take 3 <$> P.take 4
+
 -- Turn into integer pairs. A and X are converted 1, B, Y to 2 and C, Z to 3
 convert :: ByteString -> (Int, Int)
-convert bs = (c1, c2) 
-  where 
+convert bs = (c1, c2)
+  where
     c1 = (ord . BS.head) bs - 64
     c2 = (ord . BS.last) bs - 87
 
 -- This is a little faster but I don't like it
+-- {-# LANGUAGE OverloadedStrings #-}
 -- import Data.Attoparsec.ByteString.Char8 as P (Parser, many', take)
 -- import Data.ByteString.Char8 as BS (ByteString, take)
 -- import Utils (runParser)
 
 -- run :: ByteString -> (Int, Int)
 -- run input = (p1, p2)
---   where 
+--   where
 --     parsedInput = runParser parser input
 --     p1 = sum $ map fst parsedInput
 --     p2 = sum $ map snd parsedInput
@@ -54,14 +54,13 @@ convert bs = (c1, c2)
 -- parser = many' $ (evaluate . (BS.take 3)) <$> P.take 4
 
 -- evaluate :: ByteString -> (Int, Int)
--- evaluate bs 
+-- evaluate bs
 --   | bs == "A X" = (4, 3)
---   | bs == "A Y" = (8, 4)    
---   | bs == "A Z" = (3, 8)    
---   | bs == "B X" = (1, 1)    
---   | bs == "B Y" = (5, 5)    
---   | bs == "B Z" = (9, 9)    
---   | bs == "C X" = (7, 2)    
---   | bs == "C Y" = (2, 6)    
---   | otherwise = (6, 7)    
-
+--   | bs == "A Y" = (8, 4)
+--   | bs == "A Z" = (3, 8)
+--   | bs == "B X" = (1, 1)
+--   | bs == "B Y" = (5, 5)
+--   | bs == "B Z" = (9, 9)
+--   | bs == "C X" = (7, 2)
+--   | bs == "C Y" = (2, 6)
+--   | otherwise = (6, 7)
--- src/Day3.hs
+++ src/Day3.hs
@@ -1,12 +1,12 @@
-module Day3(run) where
+module Day3 (run) where
 
-import Data.Attoparsec.ByteString.Char8 as P (Parser, many1', sepBy', notChar, char)
+import Data.Attoparsec.ByteString.Char8 as P (Parser, char, many1', notChar, sepBy')
 import Data.ByteString.Char8 as BS (ByteString)
+import Data.Char (ord)
 import Utils (runParser, splitInHalf)
-import Data.Char ( ord )
 
 run :: ByteString -> (Int, Int)
-run input = (p1, p2) 
+run input = (p1, p2)
   where
     parsedInput = runParser parser input
     p1 = sum $ map (findCommonPair . splitInHalf) parsedInput
@@ -14,13 +14,13 @@ run input = (p1, p2)
 
 findCommonPair :: ([Int], [Int]) -> Int
 findCommonPair (l1, l2) = head [x | x <- l1, x `elem` l2]
-  
+
 findCommonTrios :: [[Int]] -> [Int]
-findCommonTrios (l1:l2:l3:ls) = head [x | x <- l1, x `elem` l2, x `elem` l3] : findCommonTrios ls
+findCommonTrios (l1 : l2 : l3 : ls) = head [x | x <- l1, x `elem` l2, x `elem` l3] : findCommonTrios ls
 findCommonTrios _ = []
 
 parser :: Parser [[Int]]
-parser =  parseItems `sepBy'` char '\n'
-  where 
-    parseItems = many1' $ (evaluate . ord) <$> notChar '\n'
+parser = parseItems `sepBy'` char '\n'
+  where
+    parseItems = many1' $ evaluate . ord <$> notChar '\n'
     evaluate c = if c > 96 then c - 96 else c - 38
