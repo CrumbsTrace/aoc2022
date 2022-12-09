32343057a2ea65fbd78953f804f9432a3a330e0c
CrumbsTrace <45168144+CrumbsTrace@users.noreply.github.com>
2022-12-09 16:49:32 +0100

    Start of day 9

--- src/Day9.hs
+++ src/Day9.hs
@@ -3,7 +3,53 @@
 module Day9(run) where
 
 import Utils
-import qualified Data.ByteString as BS
+import Data.ByteString (ByteString)
+import Data.Attoparsec.ByteString.Char8 (Parser, string, decimal, many', char, anyChar, notChar)
+import qualified Data.Set as Set
 
-run :: BS.ByteString -> (Int, Int)
-run input = (0, 0)
+type Positions = ((Int, Int), (Int, Int))
+
+run input = (p1, p2)
+  where 
+    instructions = runParser parser input
+    results = foldr moveAndSaveLocations (((0,0), (0,0)), Set.empty) instructions
+    p1 = length $ snd results
+    p2 = 0
+
+moveAndSaveLocations :: (Int, Int) -> (Positions, Set.Set (Int, Int)) -> (Positions, Set.Set (Int, Int))
+moveAndSaveLocations (0, 0) results = results 
+moveAndSaveLocations movement (positions, visited) = moveAndSaveLocations newMovement (newPositions, newVisited)
+  where 
+    newVisited = Set.insert t visited
+    (newMovement, newPositions@(_, t)) = move movement positions
+
+
+
+move :: (Int, Int) -> Positions -> ((Int, Int), Positions)
+move movement (h, t) = (updatedMovement, updatedPositions)
+  where 
+    updatedPositions = updatePositions (h, t) movement
+    updatedMovement = updateMovement movement
+
+updatePositions :: Positions -> (Int, Int) -> Positions
+updatePositions (h@(hx, hy), t@(tx, ty)) movement 
+  | abs ((hx - tx) + (hy - ty)) <= 1 = (takeStep h movement, t)
+  | otherwise = (takeStep h movement, h)
+
+takeStep :: (Int, Int) -> (Int, Int) -> (Int, Int)
+takeStep (px, py) (dx, dy) 
+  | dx /= 0 = (px + (dx `div` abs dx), py)
+  | otherwise = (px, py + (dy `div` abs dy))
+
+updateMovement :: (Int, Int) -> (Int, Int)
+updateMovement (dx, dy) 
+  | dx /= 0 = ((dx - (dx `div` abs dx)), dy)
+  | otherwise = (dx , dy - (dy `div` abs dy))
+
+parser :: Parser [(Int, Int)]
+parser = many' (parseInstruction <$> anyChar <* char ' ' <*> decimal <* char '\n')
+  where
+    parseInstruction 'U' n = (0, n)
+    parseInstruction 'D' n = (0, -n)
+    parseInstruction 'L' n = (-n, 0)
+    parseInstruction r n = (n, 0)
