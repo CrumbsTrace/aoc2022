366f04cd51a2be8d9a1910dcfb5c3d1741830813
CrumbsTrace <45168144+CrumbsTrace@users.noreply.github.com>
2022-12-25 02:48:30 +0100

    Some improvements

--- src/Day24.hs
+++ src/Day24.hs
@@ -23,9 +23,9 @@ navigate grid getSnacks
   | getSnacks = startRun begin end $ startRun end begin $ startRun begin end 0
   | otherwise = startRun begin end 0
   where
-    startRun start goal elapsed = go (P.singleton (manhattan start goal) (elapsed, start)) S.empty goal
-    begin = (1, 0)
-    end = (w, h + 1)
+    startRun start goal elapsed = go (P.singleton 0 (elapsed, start)) S.empty goal
+    begin = (0, -1)
+    end = (w - 1, h)
 
     w = V.length (grid ! 0) - 2
     h = V.length grid - 2
@@ -39,24 +39,25 @@ navigate grid getSnacks
 
     go queue visited goal
       | pos == goal = t
-      | (pos, t) `S.member` visited = go queue' visited goal
-      | otherwise = go newQueue (S.insert (pos, t) visited) goal
+      | otherwise = go newQueue newVisited goal
       where
         ((t, pos@(x, y)), queue') = fromJust $ P.minView queue
+        t' = t + 1
         moves =
           filter
-            (\p' -> p' == end || p' == begin || (isSafe (t + 1) p' && not (S.member (p', t + 1) visited)))
-            [(x - 1, y), (x + 1, y), (x, y + 1), (x, y - 1), (x, y)]
-        newQueue = foldr (\p' acc -> P.insert ((t + 1) + manhattan p' goal) (t + 1, p') acc) queue' moves
+            (\p' -> p' == end || p' == begin || (isSafe t' p' && not (S.member (p', t') visited)))
+            [(x - 1, y), (x + 1, y), (x, y + 1), (x, y - 1), pos]
+        newVisited = foldl (\acc p' -> S.insert (p', t') acc) visited moves
+        newQueue = foldr (\p' acc -> P.insert (t' + manhattan p' goal) (t', p') acc) queue' moves
 
     get (x, y) = (grid ! (y + 1)) ! (x + 1)
-    inBounds (x, y) = x > 0 && x <= w && y > 0 && y <= h
+    inBounds (x, y) = x >= 0 && x < w && y >= 0 && y < h
     isSafe t p@(x, y) =
       inBounds p
-        && (ups ! (x - 1)) ! (((y + t) - 1) `mod` h)
-        && (downs ! (x - 1)) ! (((y - t) - 1) `mod` h)
-        && (lefts ! (y - 1)) ! (((x + t) - 1) `mod` w)
-        && (rights ! (y - 1)) ! (((x - t) - 1) `mod` w)
+        && (lefts ! y) ! ((x + t) `mod` w)
+        && (rights ! y) ! ((x - t) `mod` w)
+        && (ups ! x) ! ((y + t) `mod` h)
+        && (downs ! x) ! ((y - t) `mod` h)
 
 manhattan :: Point -> Point -> Int
 manhattan (x, y) (x2, y2) = abs (x2 - x) + abs (y2 - y)
